1193727f1c1786b9ef6ef588017ceb95a6bd94a2
Md.Solaiman <md.solaiman366882@gmail.com>
2023-09-15 02:30:55 +0600

    Card design completed

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,14 +1,30 @@
 import image from"../../assets/background.png"
-const Card = () => {
+const Card = ({card}) => {
+
+    const{name,age,profession,role,actor_name,salary,powers,profile_img}=card;
+
     return (
         <div className="card">
             <div className="card-img">
-                <img src={image} alt="" />
+                <img src={profile_img} alt="" />
             </div>
             <div className="card-body">
                 <div className="title">
-                    <h2 className="text-2xl">Tony Stark</h2>
+                    <h2 className="">{name}</h2>
                 </div>
+                <div className="details grid grid-cols-2 gap-2">
+                    <p className="age">Age: {age}</p>
+                    <p className="profession">Prof:{profession}</p>
+                    <p className="salary">Salary:{salary}</p>
+                    <p className="actor">Actor:{actor_name}</p>
+                </div>
+                <div className="powers">
+                    <h3 className=" font-bold">Power:</h3>
+                    {
+                        powers.map((power,idx) => <p key={idx}>{power},</p>)
+                        }
+                </div>
+                <button className="btn">Select</button>
             </div>
         </div>
     );
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -19,7 +19,7 @@ const Cards = () => {
         <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
             {/* <h2 className=" text-center text-3xl font-bold text-white">All Cards will display here</h2> */}
             {
-                cards.map(() => <Card></Card>)
+                cards.map((card) => <Card card={card} key={card.id}></Card>)
             }
         </div>
     );
